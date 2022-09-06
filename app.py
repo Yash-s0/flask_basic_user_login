e106ffa7563be2a78e8c210d8a92f5dad1c90586
Yash Sharma <yas21606@gmail.com>
2022-09-06 09:06:06 +0530

    Working API with updated requirements file

--- app.py
+++ app.py
@@ -1,24 +1,51 @@
-# redis dekhna h
-# how to create and verify bearer token
-
-
 from flask import Flask, render_template
 from flask_restful import Resource, Api, request
 from passlib.hash import sha256_crypt
 from sqlalchemy import create_engine, Column, Integer, String, inspect
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
+import datetime
+import jwt
 
 
 app = Flask(__name__)
 api = Api(app)
-
-
-users = {}
+SECRET_KEY = "mysecret"
 
 
 dbEngine = create_engine("sqlite:///site.db")
 Session = sessionmaker(bind=dbEngine)
+
+
+def encode_auth_token(user_id):
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    # try:
+    payload = {
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=60),
+        "iat": datetime.datetime.utcnow(),
+        "sub": user_id,
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    # except Exception as e:
+    #     print(e)
+
+
+def decode_auth_token(auth_token):
+    """
+    Decodes the auth token
+    :param auth_token:
+    :return: integer|string
+    """
+    try:
+        payload = jwt.decode(auth_token, SECRET_KEY, algorithms=["HS256"])
+        return {"success": True, "username": payload["sub"]}
+    except jwt.ExpiredSignatureError:
+        return {"success": False, "message": "Signature expired. Please log in again."}
+    except jwt.InvalidTokenError:
+        return {"success": False, "message": "Signature expired. Please log in again."}
 
 
 @as_declarative()
@@ -49,20 +76,21 @@ class RegisterUser(Resource):
         username = args.get("username")
         password = args.get("password")
         hashed_pass = sha256_crypt.encrypt(password)
-        # to decode the password.
-        # org_pass = sha256_crypt.verify(password, hashed_pass)
 
         if username is None or password is None:
             return {"success": False, "message": "Missing required params"}, 400
+        db = Session()
 
-        if username in users:
+        user = db.query(User).filter_by(username=username).first()
+        if user:
             return {"success": False, "message": "User already exists."}, 409
         # users[username] = password
 
         new_user = User(username=username, password=hashed_pass)
-        db = Session()
+
         db.add(new_user)
         db.commit()
+        db.close()
 
         return {
             "success": True,
@@ -70,19 +98,56 @@ class RegisterUser(Resource):
         }
 
 
-class AllUsers(Resource):
-    # WORKING GET ALL USERS
-    def get(self):
+class LogInUser(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data["username"]
+        password_login = data["password"]
+
         db = Session()
-        all_users = db.query(User).all()
-        all_users = [user._asdict() for user in all_users]
 
-        return [name["username"] for name in all_users]
+        user = db.query(User).filter_by(username=username).first()
+        if not user:
+            return {"success": False, "message": "User does not exist."}
+
+        # print(user.username)
+
+        verify_password = sha256_crypt.verify(password_login, user.password)
+
+        if not verify_password:
+            return {"success": False, "message": "Wrong password."}
+
+        print(user.username)
+        bearer_token = encode_auth_token(user.username)
+        print(bearer_token)
+        db.close()
+        return {"succes": True, "bearer_token": bearer_token}
 
 
-api.add_resource(HealthCheck, "/health_check")
+class UserInfo(Resource):
+    # WORKING GET ALL USERS
+    # DO NOT CHANGE ANYTHING
+    def get(self):
+        token = request.headers.get("Authorization")
+        if token is None:
+            return {"success": False, "message": "Session expried, please login"}, 404
+
+        bearer_token = token.split("Bearer ")[1]
+        verify = decode_auth_token(bearer_token)
+        if "success" in verify and verify["success"] is False:
+            return verify
+
+        db = Session()
+        user = db.query(User).filter_by(username=verify["username"]).first()
+        data = user._asdict()
+        del data["password"]
+        return data
+
+
+api.add_resource(HealthCheck, "/health-check")
 api.add_resource(RegisterUser, "/register")
-api.add_resource(AllUsers, "/get_all_users")
+api.add_resource(UserInfo, "/user-info")
+api.add_resource(LogInUser, "/login")
 
 
 if __name__ == "__main__":
